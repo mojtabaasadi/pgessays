440c155d46d9e71148b6daefd5b90cd238ee8b89
mojtaba asadi <arian.4339@gmail.com>
2026-02-13 12:52:32 +0330

    all pages are static

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next';
 import ArticleContent from './content.mdx'
-
+export const dynamic = "force-static";
 export const metadata: Metadata = {
   title: "مقالات ",
   description: "لیست ترجمه مفالات پال گرام به فارسی",
